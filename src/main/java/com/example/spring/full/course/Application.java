af3f5a97988092df735007ff97c93c410fa634f9
Olga <olganeboga209@gmail.com>
2024-04-19 23:36:50 +0600

    API layer created

--- src/main/java/com/example/spring/full/course/Application.java
+++ src/main/java/com/example/spring/full/course/Application.java
@@ -1,32 +1,16 @@
 package com.example.spring.full.course;
 
-import com.example.spring.full.course.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
+
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(1L,
-						"Noa",
-						"noa@gmal.com",
-						LocalDate.of(2005, Month.JULY, 9),
-						18
-				)
-		);
-	}
+
 }
